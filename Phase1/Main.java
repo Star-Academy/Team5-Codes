cec00efaa25082e74fecd190b26ee48453f0b3ba
FarshidNooshi <farshidnooshi726@aut.ac.ir>
2020-08-01 16:14:57 +0430

    testing the name

--- Phase1/Main.java
+++ Phase1/Main.java
@@ -1,7 +1,9 @@
+import javax.sound.midi.Soundbank;
+
 import jdk.internal.vm.PostVMInitHook;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("test");
     }
 }
