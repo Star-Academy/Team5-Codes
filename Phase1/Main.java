21d626ecca88354fa97f5aed946de2504a5001eb
FarshidNooshi <farshidnooshi726@aut.ac.ir>
2020-08-02 16:01:15 +0430

    section 4 added
    Signed-off-by: FarshidNooshi <farshidnooshi726@aut.ac.ir>

--- Phase1/Main.java
+++ Phase1/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -15,17 +16,50 @@ public class Main {
  
         System.out.println("Enter the phrase to search for");
         String input = scanner.nextLine();
-        Set<String> set = new HashSet<String>();
+        Set<String> ans = new HashSet<>();
+        Set<String> set1 = new HashSet<String>();
+        Set<String> set2 = new HashSet<String>();
+        boolean flag = false;
         String[] splitInput = input.split("\\s");
         for (String str : splitInput) {
-            if (tokenizer.getHashMap().containsKey(str.toLowerCase()))
-                set.addAll(tokenizer.getHashMap().get(str.toLowerCase()));
+            String str2 = str.substring(1);
+            switch (str.charAt(0)) {
+                case '+':
+                if (tokenizer.getHashMap().containsKey(str2.toLowerCase()))
+                    ans.addAll(tokenizer.getHashMap().get(str2.toLowerCase()));
+                    break;
+                case '-':
+                if (tokenizer.getHashMap().containsKey(str2.toLowerCase()))
+                    set1.addAll(tokenizer.getHashMap().get(str2.toLowerCase()));
+                    break;
+                default:
+                    if (set2.isEmpty() && tokenizer.getHashMap().containsKey(str2.toLowerCase())) {
+                        set2.addAll(tokenizer.getHashMap().get(str2.toLowerCase()));
+                        break;
+                    }
+                    Set<String> temp = new HashSet<>();
+                    ArrayList<String> tmp = tokenizer.getHashMap().get(str2.toLowerCase());
+                    if (tmp == null) {
+                        flag = true;
+                        break;
+                    }
+                    for (String string : tmp)
+                        if (set2.contains(string))
+                            temp.add(string);
+                    set2 = temp; 
+                    break;
+            }
         }
-        if (set.isEmpty()) {
+        if (!flag)
+            ans.addAll(set2);
+        set2.forEach((k) -> {
+            ans.remove(k);
+        });
+        if (ans.isEmpty()) {
             System.out.println("search un available");
             System.exit(0);
         } else {
-            set.forEach((k -> {
+            ans.forEach((k -> {
                 System.out.println(k);
             }));
         }
