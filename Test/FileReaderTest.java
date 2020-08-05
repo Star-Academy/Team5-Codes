ca455e5d80e900935455c89f913f07b017bfe285
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-05 14:47:51 +0430

    create test package

--- Test/FileReaderTest.java
+++ Test/FileReaderTest.java
@@ -1,12 +1,22 @@
 import java.io.File;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.rules.ExpectedException;
 
 public class FileReaderTest {
 
     private final FileReader fileReader = new FileReader();
+    @Rule
+    private ExpectedException expectedException = ExpectException.none();
 
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIndexOutOfBoundsException() {
+        list.get(0);
+    }
+
+    
     public void getWordsInDocumentTest() {
 
     }
