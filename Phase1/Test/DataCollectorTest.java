7fee985cea0743363784ebd8712e9f7f5495e6c1
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-07 10:05:02 +0430

    ManagerTest created

--- Phase1/Test/DataCollectorTest.java
+++ Phase1/Test/DataCollectorTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Test;
 import classes.*;
---
+++ Phase1/Test/ManagerTest.java
@@ -0,0 +1,8 @@
+package Test;
+
+import org.junit.Test;
+
+public class ManagerTest {
+    @Test
+    
+}
