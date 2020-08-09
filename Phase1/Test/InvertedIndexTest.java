6b0ad45c055959ac82cb933ee73e9264a774ac97
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-09 10:54:18 +0430

    test

--- Phase1/Test/InvertedIndexTest.java
+++ Phase1/Test/InvertedIndexTest.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import classes.DataCollector;
@@ -34,7 +33,6 @@ public class InvertedIndexTest {
         assertEquals(expectedValue, actualValue);
     }
 
-    @Ignore
     @Test
     public void testInitMethodMultiDoc() {
         invertedIndex.init(dataCollector);
