4876e3c31d9a5e701ef4065f4e66a34dbdced535
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-11 17:49:44 +0430

    test

--- Phase4/Program.cs
+++ Phase4/Program.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Phase4.Database;
-using System.Text.Json;
 using Phase4.Models;
 
 namespace Team5_Codes
 {
     class Program
     {
-        private static List<Student> topThree = new List<Student>();
+        private static List<Student> topThree = new List<Student>(3);
 
         static void Main(string[] args)
         {
             JsonFile studentFile = new StudentJsonFile("DataFiles\\Students.json");
             JsonFile ScoreFile = new ScoreJsonFile("DataFiles\\Scores.json");
-            ShowBestGrades();
+            FindBestGrades();
+            Console.WriteLine(topThree[0].Average);
+            Console.WriteLine(topThree[1].Average);
+            Console.WriteLine(topThree[2].Average);
         }
 
-        private static void ShowBestGrades()
+        private static void FindBestGrades()
         {
             foreach (var student in Student.GetAllStudent())
             {
@@ -28,14 +29,54 @@ namespace Team5_Codes
                 }
                 else
                 {
-                    
+                    if (topThree[2].Average < student.Average)
+                    {
+                        return;
+                    }
+                    else if (topThree[1].Average < student.Average)
+                    {
+                        AddTopStudent(student, 2);
+                    }
+                    else if (topThree[0].Average < student.Average)
+                    {
+                        AddTopStudent(student, 1);
+                    }
+                    else
+                    {
+                        AddTopStudent(student, 0);
+                    }
                 }
             }
         }
-
+        private static void AddTopStudent(Student student, int index)
+        {
+            topThree[index] = student;
+        }
         private static void AddTopStudent(Student student)
         {
-
+            if (topThree.Capacity == 0) {
+                topThree[0] = student;
+            } else if (topThree.Capacity == 1) {
+                if (topThree[0].Average > student.Average) {
+                    topThree[1] = student;
+                } else {
+                    var temp = topThree[0];
+                    topThree[0] = student;
+                    topThree[1] = temp;
+                }
+            } else {
+                if (topThree[1].Average > student.Average) {
+                    topThree[2] = student;
+                } else if (topThree[0].Average > student.Average) {
+                    var temp = topThree[1];
+                    topThree[1] = student;
+                    topThree[2] = temp;
+                } else {
+                    topThree[2] = topThree[1];
+                    topThree[1] = topThree[0];
+                    topThree[0] = student;
+                }
+            }
         }
     }
 }
