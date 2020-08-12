1825383b544597ae069b5f317e0b6f045ca3a4fb
Farshid <farshidnooshi726@aut.ac.ir>
2020-08-12 11:44:04 +0430

    list updated

--- Phase4/Program.cs
+++ Phase4/Program.cs
@@ -2,30 +2,40 @@
 using System.Collections.Generic;
 using Phase4.Database;
 using Phase4.Models;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 
 namespace Team5_Codes
 {
     class Program
     {
-        private static Student[] topStudents;
-        // private const int topNumber = 3;
+        private const int NumberOfWanted = 3;
+        private static Student[] topStudents = new Student[NumberOfWanted];
+
         public static void Main(string[] args)
         {
-            JsonFile studentFile = new StudentJsonFile("DataFiles\\Students.json");
-            JsonFile ScoreFile = new ScoreJsonFile("DataFiles\\Scores.json");
+            
+            JsonFile studentFile = new StudentJsonFile(@"\DataFiles\Students.json");
+            JsonFile ScoreFile = new ScoreJsonFile(@"\DataFiles\Scores.json");
 
-            topStudents =  new Student[3];
+            FindBestGrades();
 
-            List<Student> allStudent = Student.GetAllStudent();
-            allStudent.Sort(delegate (Student std1, Student std2)
+            for (int i = 0; i < NumberOfWanted; i++)
             {
-                return std2.Average.CompareTo(std1.Average);
-            });
-            topStudents = allStudent.ToArray();
+                Console.WriteLine("" + (i + 1) + "st:\n" +
+                topStudents[i].FirstName + " " + topStudents[i].LastName + "\n" + topStudents[i].Average);
+            }
+            
+        }
 
-            for (int i = 0; i < 3 && i < topStudents.Length; i++)
+        private static void FindBestGrades()
+        {
+            var list = Student.GetAllStudent();
+            list = list.OrderBy(x => x.Average).ToList();
+            for (int i = 0; i < NumberOfWanted; i++)
             {
-                Console.WriteLine((i + 1) + ":\n" + topStudents[0].FirstName + " " + topStudents[0].LastName + "\n" + topStudents[0].Average);
+                topStudents[i] = list[i];
             }
         }
     }
