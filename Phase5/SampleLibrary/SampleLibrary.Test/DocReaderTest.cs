5ddbbf49201cba85e1290bd6e24706cdbbf23755
Farshid <farshidnooshi726@aut.ac.ir>
2020-08-13 15:21:23 +0430

    testing

--- Phase5/SampleLibrary/SampleLibrary.Test/DocReaderTest.cs
+++ Phase5/SampleLibrary/SampleLibrary.Test/DocReaderTest.cs
@@ -1,13 +1,37 @@
-﻿using Xunit;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace SampleLibrary.Test
 {
     public class DocReaderTest
     {
-        [Fact]
-        public void test2()
+        private DocReader reader;
+        [TestInitialize]
+        private void init()
         {
-             Assert.Equal(1, 2);
+            reader = DocReader.generate("Docs");
         }
+
+        [Fact]
+        public void test1() => Xunit.Assert.Equal(reader.DocumentWords.Count, 1000);
+
+        [Fact]
+        public void test2() => Xunit.Assert.Equal(reader.root, @"C:\Users\Farshid726\Desktop\Codes\CodeStar\Team5-Codes\Phase5\Docs");
+        
+        [Fact]
+        public void test3() => Xunit.Assert.Equal(reader.DocumentWords["58044"].Contains("announcing"), true);
+
+        [Fact]
+        public void test4() => Xunit.Assert.Equal(reader.DocumentWords["58044"].Contains("presidentia"), true);
+
+        [Fact]
+        public void test5() => Xunit.Assert.Equal(reader.DocumentWords["58057"].Contains("when"), true);
+
+        [Fact]
+        public void test6()
+        {
+            Xunit.Assert.Equal(reader.DocumentWords["58057"].Contains("ishalla"), false);
+        }
+
     }
 }
--- Phase5/SampleLibrary/SampleLibrary/DocReader.cs
+++ Phase5/SampleLibrary/SampleLibrary/DocReader.cs
@@ -1,6 +1,28 @@
-﻿namespace SampleLibrary
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SampleLibrary
 {
     public class DocReader
     {
+        private static DocReader docReader = null;
+        public string root { get; set; }
+        public Dictionary<string, List<string>> DocumentWords { get; set; }
+        public static DocReader generate(string v)
+        {
+            if (docReader == null)
+                docReader = new DocReader(v);
+            return docReader;
+        }
+
+        private DocReader()
+        {
+        }
+
+        private DocReader(String v)
+        {
+            root = Path.GetFullPath(v);
+        }
     }
 }
