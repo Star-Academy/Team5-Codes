f0578b9b8970ccd73d59325f0d1cdd7c0b15cf2e
Farshid <farshidnooshi726@aut.ac.ir>
2020-08-15 15:02:50 +0430

    implemented now debugging the inputReader class

--- Phase5/SampleLibrary/SampleLibrary/UserInputReader.cs
+++ Phase5/SampleLibrary/SampleLibrary/UserInputReader.cs
@@ -1,26 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SampleLibrary
 {
     public class UserInputReader
     {
 
-        public string Input { get; set; }
+        private readonly char[] tokens = { ' ', ',', ';', '-', '(', ')', '\\', '@', '[', ']', '<', '>' };
+        public List<string> Input { get; set; }
 
         public UserInputReader()
         {
-            Input = Console.ReadLine();
+            string text = Console.ReadLine();
+            Input = new List<string>(text.Split(tokens).Select(p => p.ToLower()).ToList<string>());
         }
 
         public string[][] processInput(string s)
         {
-            //List<string> positiveSignedWords = takeWords('-');
-            //List<string> negativeSignedWords = takeWords('+');
-            //List<string> unSignedWords = takeWords(null);
-            //return new string[][] { unSignedWords, positiveSignedWords, negativeSignedWords };
-            return null;
+            List<string> positiveSignedWords = takeWords('-');
+            List<string> negativeSignedWords = takeWords('+');
+            List<string> unSignedWords = takeWords('\0');
+            return new string[][] { unSignedWords.ToArray(), positiveSignedWords.ToArray(), negativeSignedWords.ToArray() };
+        }
 
+        private List<string> takeWords(char token)
+        {
+            List<string> ret = new List<string>();
+            foreach(string word in Input)
+            {
+                if (word[0] == token)
+                    ret.Add(word);
+            }
+            return ret;
         }
     }
 }
