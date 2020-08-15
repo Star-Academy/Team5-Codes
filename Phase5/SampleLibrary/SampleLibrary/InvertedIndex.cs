f8f9749b5713f3ec3a4f10f81747bb6e155736aa
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-15 10:03:42 +0430

    write method in InvertedIndex

--- Phase5/SampleLibrary/SampleLibrary/InvertedIndex.cs
+++ Phase5/SampleLibrary/SampleLibrary/InvertedIndex.cs
@@ -1,6 +1,32 @@
-﻿namespace SampleLibrary
-{
-    public class InvertedIndex
-    {
+﻿using System.Collections.Generic;
+
+namespace SampleLibrary {
+    public class InvertedIndex {
+
+        private Dictionary<string, List<string>> wordsInDocs;
+
+        public Dictionary<string, List<string>> WordToDocs { get; set; }
+
+        public InvertedIndex (Dictionary<string, List<string>> wordsInDocs) {
+            this.wordsInDocs = wordsInDocs;
+            Init ();
+        }
+
+        private void Init () {
+            foreach (var item in wordsInDocs) {
+                foreach (var word in item.Value) {
+                    WordToDocs = AddWordToDictionary (word, item.Key);
+                }
+            }
+        }
+
+        public Dictionary<string, List<string>> AddWordToDictionary (string word, string doc) {
+            if (WordsToDoc.Contain (word)) {
+                WordToDocs[word].Add(doc);
+            } else {
+                WordToDocs.Add(word, new List<string>(){"doc"});
+            }
+            return WordToDocs;
+        }
     }
 }
