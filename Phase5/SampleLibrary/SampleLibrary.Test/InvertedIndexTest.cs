16d5adcdcbab1f0259970806eaedd3573b4b9dde
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-15 09:19:48 +0430

    write InvertedIndexTest

--- Phase5/SampleLibrary/SampleLibrary.Test/InvertedIndexTest.cs
+++ Phase5/SampleLibrary/SampleLibrary.Test/InvertedIndexTest.cs
@@ -1,6 +1,42 @@
-﻿namespace SampleLibrary.Test
-{
-    public class InvertedIndexTest
-    {
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using Xunit;
+
+namespace SampleLibrary.Test {
+    public class InvertedIndexTest {
+
+        private InvertedIndex invertedIndex = new InvertedIndex ();
+        private Dictionary<string, List<string>> wordToDoc;
+        private List<string> fisrtWordDocs;
+
+        [Fact]
+        public void AddNewWordToDictionary () {
+            wordToDoc; = new Dictionary<string, List<string>> ();
+            fisrtWordDocs = new List<string> ();
+
+            Dictionary<string, List<string>> actualValue = invertedIndex.AddWordToDictionary ("word_1", "doc_1");
+            Dictionary<string, List<string>> expectedValue = new Dictionary<string, string> () {
+                "word_1",
+                new  List<string>() {
+                    "doc_1"
+                }
+            }
+            Assert.Equal (actualValue, expectedValue);
+        }
+
+        [Fact]
+        public void AddExistedWordToDictionary () {
+            Dictionary<string, List<string>> actualValue = invertedIndex.AddWordToDictionary ("word_1", "doc_2");
+            Dictionary<string, List<string>> expectedValue = new Dictionary<string, string> () {
+                "word_1",
+                new  List<string>() {
+                    "doc_1",
+                    "doc_2"
+                }
+            }
+            Assert.Equal (actualValue, expectedValue);
+        }
     }
+}
 }
