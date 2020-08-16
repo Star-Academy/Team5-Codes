bc68d79cec8d6d1d3358fc4fe17d92d5ee8db4ae
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-16 14:49:42 +0430

    delete WriterTest

--- Phase5/SampleLibrary/SampleLibrary.Test/InvertedIndexTest.cs
+++ Phase5/SampleLibrary/SampleLibrary.Test/InvertedIndexTest.cs
@@ -47,10 +47,8 @@ namespace SampleLibrary.Test
         [Fact]
         public void AddExistedWordToDictionary()
         {
-            Dictionary<string, HashSet<string>> actualValue;
-            actualValue = invertedIndex.AddWordToTokenize("word_1", "doc_3");
-            Dictionary<string, HashSet<string>> expectedValue;
-            expectedValue = new Dictionary<string, HashSet<string>>() {
+            var actualValue = invertedIndex.AddWordToTokenize("word_1", "doc_3");
+            var expectedValue = new Dictionary<string, HashSet<string>>() {
                  { "word_1", new HashSet<string> () { "doc_1", "doc_2", "doc_3"} },
                  { "word_2", new HashSet<string> () { "doc_1"} }
             };
