2de60da7cd8831923d6d0f8751da12b9f933fcd1
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-19 15:02:28 +0430

    create phase8

---
+++ C#/Phase8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Phase8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
