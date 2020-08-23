28b847a5e3b4c5791118532ffa3ef6f017ae3953
Farshid <farshidnooshi726@aut.ac.ir>
2020-08-23 17:49:10 +0430

    validator is ok

--- C#/Phase8/ResponseValidator.cs
+++ C#/Phase8/ResponseValidator.cs
@@ -1,4 +1,5 @@
 ﻿using Nest;
+using System;
 
 namespace Phase8
 {
@@ -6,12 +7,23 @@ namespace Phase8
     {
         public static void Validate(ISearchResponse<Person> response)
         {
-            if (response.ApiCall.Success)
+            if (response.ApiCall.Success && response.IsValid)
             {
+                Console.WriteLine("request took " + response.Took + "ms and it has " + response.Total + "results.");
                 Program.ShowResult(response.Documents);
                 return;
             } 
+            if (response.OriginalException != null)
+                Console.WriteLine("Exception occured while processing the request.");
 
+            if (response.ServerError != null)
+                Console.WriteLine("error occured in the server side of the process.");
+
+            if (response.TerminatedEarly)
+                Console.WriteLine("request terminated earlier than it was supposed to.");
+
+            if (response.TimedOut)
+                Console.WriteLine("request timed out!");
         }
     }
 }
