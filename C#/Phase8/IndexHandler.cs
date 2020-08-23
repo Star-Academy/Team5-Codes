3e814fc934186deafca82d303cd9eea3d786e5b2
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-23 09:35:29 +0430

    Person mapping

--- C#/Phase8/IndexHandler.cs
+++ C#/Phase8/IndexHandler.cs
@@ -10,14 +10,46 @@ namespace Phase8 {
             this.client = ElasticSearch.GetClient ();
         }
 
+        ///// Mapping is based on Person class /////
         public void CreateIndex (string index) {
-
             var createIndexResponse = client.Indices.Create (index, c => c
-                .Map<T> (m => m
-                    .AutoMap ()
+                .Map<Person> (map => map
+                    .Properties (pr => pr
+                        .Number (num => num
+                            .Name (n => n.Age)
+                        )
+                        .Text (t => t
+                            .Name (n => n.EyeColor)
+                        )
+                        .Text (t => t
+                            .Name (n => n.Name)
+                        )
+                        .Text (t => t
+                            .Name (n => n.Gender)
+                        )
+                        .Text (t => t
+                            .Name (n => n.Company)
+                        )
+                        .Text (t => t
+                            .Name (n => n.Phone)
+                        )
+                        .Text (t => t
+                            .Name (n => n.Address)
+                        )
+                        .Text (t => t
+                            .Name (n => n.About)
+                        )
+                        .Date (d => d
+                            .Name (n => n.RegistrationDate)
+                        )
+                        .GeoPoint (g => g
+                            .Name (n => n.Location)
+                        )
+                    )
                 )
             );
         }
+
         public void AddDocToIndex (string index, List<T> list) {
             var bulkDescriptor = new BulkDescriptor ();
             foreach (var person in list) {
