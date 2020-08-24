456b8e3078da9e5cd02f3a1221689b24561f1699
Farshid <farshidnooshi726@aut.ac.ir>
2020-08-24 14:11:46 +0430

    validator modified

--- C#/Phase8/ResponseValidator.cs
+++ C#/Phase8/ResponseValidator.cs
@@ -6,21 +6,23 @@ namespace Phase8
     {
         public static void Validate(ResponseBase response)
         {
-            if (response.OriginalException != null)
-                Output.Throw("Exception occurred while processing the request.");
-
-            if (response.ServerError != null)
-                Output.Throw("error occurred in the server side of the process.");
-
             if (response is ISearchResponse<Person>)
             {
                 var res = (ISearchResponse<Person>)response;
+                Output.Write("only " + res.Total + " results found");
                 if (res.TerminatedEarly)
                     Output.Throw("request terminated earlier than it was supposed to.");
 
                 if (res.TimedOut)
                     Output.Throw("request timed out!");
             }
+
+            if (response.OriginalException != null)
+                Output.Throw("Exception occurred while processing the request.");
+
+            if (response.ServerError != null)
+                Output.Throw("error occurred in the server side of the process.");
+
         }
 
         public static bool Check(ResponseBase response)
