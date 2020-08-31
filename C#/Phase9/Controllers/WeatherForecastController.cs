c5ac00bfd1e0a2c91a1dacf4bcc1d551b17cecd2
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-31 12:35:05 +0430

    Tag

--- C#/Phase9/Controllers/WeatherForecastController.cs
+++ C#/Phase9/Controllers/WeatherForecastController.cs
@@ -9,16 +9,16 @@ namespace Phase9.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class MainController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<MainController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public MainController(ILogger<MainController> logger)
         {
             _logger = logger;
         }
