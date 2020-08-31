8b7880b45fb3126fbaefba2b3135549484eca77d
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-08-31 12:54:48 +0430

    change names

---
+++ C#/Phase9/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Phase9.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MainController : ControllerBase
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{query}")]
+        public string Get(string query)
+        {
+            return $"{query}";
+        }
+    }
+}
