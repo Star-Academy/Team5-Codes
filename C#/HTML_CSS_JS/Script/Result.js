20c627a0cdb394d4afc91165ae79ddb834157aa8
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-09-06 15:59:25 +0430

    split resultd

--- C#/HTML_CSS_JS/Script/Result.js
+++ C#/HTML_CSS_JS/Script/Result.js
@@ -1,5 +1,20 @@
 function update(Response) {
-    document.getElementById('results').innerText = Response;
-    // document.getElementById('results').innerHTML = "<div>" + Response + "</div>";
+    text = splitResponse(Response);
+
+    var div = document.createElement('div');
+    div.innerHTML = text;
+    var parent = document.getElementById('results');
+    parent.removeChild(parent.lastChild);
+    parent.appendChild(div);
     scrollTo({ top: document.getElementById('results').offsetTop, behavior: 'smooth' });
+}
+
+function splitResponse(Response) {
+    resultItems = Response.split("Age:");
+    var text = '';
+    resultItems.forEach(element => {
+        if (element != '')
+            text += "Age:" + element + "<hr>";
+    });
+    return text;
 }
