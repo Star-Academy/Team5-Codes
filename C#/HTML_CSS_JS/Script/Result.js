772546497cb5c7c934a0375a73b05424d04b195a
soleyman79 <59196723+soleyman79@users.noreply.github.com>
2020-09-06 13:25:48 +0430

    change result page

--- C#/HTML_CSS_JS/Script/Result.js
+++ C#/HTML_CSS_JS/Script/Result.js
@@ -1,4 +1,5 @@
 function update(Response) {
     document.getElementById('results').innerText = Response;
+    // document.getElementById('results').innerHTML = "<div>" + Response + "</div>";
     scrollTo({ top: document.getElementById('results').offsetTop, behavior: 'smooth' });
 }
