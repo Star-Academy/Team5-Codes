dbbf6fe33e50a5d19fbe206c9fb5da2a39d1a056
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-07 13:10:50 +0430

    code doesn't work, just a save

--- C#/web-app/src/app/app.component.ts
+++ C#/web-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web-app';
+  Click(event) {
+      if (event.key === 'Enter') {
+        const searchWrapper = 
+        const request = searchWrapper.value;
+        const xHttp = new XMLHttpRequest();
+        xHttp.onreadystatechange = function () {
+          if (this.readyState === 4 && this.status === 200) {
+            localStorage.setItem("response", this.responseText);
+            if (window.location.href.includes("Result.html")) {
+              update();
+            } else {
+              window.location.replace("Result.html");
+            }
+          }
+        };
+        xHttp.open('POST', 'https://localhost:5001/Main/Get');
+        xHttp.setRequestHeader('Content-Type', 'application/json');
+        xHttp.send(JSON.stringify(request));
+      }
+  }
 }
