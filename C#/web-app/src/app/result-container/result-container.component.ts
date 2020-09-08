faf76cdd5a17fc958bcef2c39ceec1910b063af0
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-08 12:31:38 +0430

    changes for showing the result

--- C#/web-app/src/app/result-container/result-container.component.ts
+++ C#/web-app/src/app/result-container/result-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-result-container',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultContainerComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  public results: string[]; // bayad tokenize beshe
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
