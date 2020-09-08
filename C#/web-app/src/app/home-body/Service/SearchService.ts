d7379a0cf7126022b045ab62dfe71d7fdf034263
Farshid <farshidnooshi726@aut.ac.ir>
2020-09-08 10:14:21 +0430

    good morning starting the work!

--- C#/web-app/src/app/home-body/Service/SearchService.ts
+++ C#/web-app/src/app/home-body/Service/SearchService.ts
@@ -8,7 +8,7 @@ export class SearchService {
 
   // will return a string we should add a token at server side and tokenize it after receiving it
   public async searchRequest(searchKey: string): Promise<string> {
-    alert(searchKey + ' received the request doesn\'t work');
+    alert(searchKey + ' received the request doesn\'t work'); 
     return new Promise<string>((resolve) => {
       this.http.post('https://localhost:5001/Main/Get', searchKey, {headers: {'Content-Type': 'application/json', accept: '*/*'}})
         .subscribe((result: string) => {
